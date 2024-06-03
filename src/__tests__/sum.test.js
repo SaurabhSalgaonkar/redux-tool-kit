ecddd075a0a10240dfe7f6a2eab852009ef1774e
SaurabhSalgaonkar <60724162+SaurabhSalgaonkar@users.noreply.github.com>
2024-06-03 08:15:04 +0000

    Test setup

---
+++ src/__tests__/sum.test.js
@@ -0,0 +1,6 @@
+import { sum } from "../utils/sum";
+
+it('some-test-name', () => {
+    const result = sum(1,2);
+    expect(result).toBe(3);
+});
--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 export const About = () => {
+ const [ click, setClick ] = useState('not clicked');   
+
+    const handleOnclickSubmit = (e) => {
+        e.preventDefault();
+        console.log('I submitted');
+        setClick('clicked');
+    }
+
+    console.log(click);
 
     return (
         <>
-            About
+            <h1>About</h1>
+            <form>
+                <input type="text" placeholder="tell me what you want to enquire"/>
+                <input type="text" placeholder="feedback"/>
+                <button type="submit" onClick={handleOnclickSubmit}>Submit</button>
+                <button type="submit" onClick={handleOnclickSubmit}>Submit New Button</button>
+            </form>
         </>
     )
 }
